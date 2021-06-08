5098ff698423cab132f529ba8840e6156a52df08
asmatar <deruelle.arthur@gmail.com>
2021-06-08 11:16:04 +0200

    fetching data on the new page depends on id

--- src/components/UpdateRestaurant.js
+++ src/components/UpdateRestaurant.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import RestaurantFinder from '../api/RestaurantFinder';
+import { RestaurantContext } from '../context/restaurantContext';
 
 const UpdateRestaurant = (props) => {
-    
+    const {restaurants} = useContext(RestaurantContext);
     const {id} = useParams()
     const [name, setName] = useState('')
     const [location, setLocation] = useState('')
     const [price, setPrice] = useState('Price range')
 
+    useEffect(() => {
+       const fetchData = async() => {
+           const response = await RestaurantFinder.get(`/${id}`)
+        //    console.log(response.data.data)
+           setName(response.data.data.restaurants.name)
+           setLocation(response.data.data.restaurants.location)
+           setPrice(response.data.data.restaurants.price)
+       }
+       fetchData()
+    }, [])
+
+
     return (
         <div>
+            {/* <h1>{restaurants[0].name}</h1> */}
             <form action="">
                 <div className="form-group">
                     <label htmlFor="name">Name</label>
