35d4eb68381a5eaf6afaa8337ef451105e184283
asmatar <deruelle.arthur@gmail.com>
2021-06-12 16:56:29 +0200

    front changement axios

--- src/api/RestaurantFinder.js
+++ src/api/RestaurantFinder.js
@@ -1,3 +1,5 @@
 import axios from 'axios';
 
-export default axios.create({baseURL:'http://localhost:3001/api/v1/restaurants'})
+// export default axios.create({baseURL:'http://localhost:3001/api/v1/restaurants'})
+
+export default axios.create({baseURL:'art-drl-back-rest.netlify.app/api/v1/restaurants'})
