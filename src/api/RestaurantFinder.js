66b3420f7f6fb557c1eda323c1601de539764e84
asmatar <deruelle.arthur@gmail.com>
2021-06-17 12:15:02 +0200

    .

--- src/api/RestaurantFinder.js
+++ src/api/RestaurantFinder.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
 // export default axios.create({baseURL:'http://localhost:3001/api/v1/restaurants'})
+export default axios.create({baseURL:'https://arthur-drl-restaurant.netlify.app'})
 
-export default axios.create({baseURL:'art-drl-back-rest.netlify.app/api/v1/restaurants'})
+// export default axios.create({baseURL:'art-drl-back-rest.netlify.app/api/v1/restaurants'})
+
+//rest-euw.herokuapp.com
+
+// http://localhost:2500/api/v1/restaurants
+
+// https://rest-euw.herokuapp.com/
