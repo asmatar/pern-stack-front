4cfaa889c1b5fac410eeb333b5bab09920af633a
asmatar <deruelle.arthur@gmail.com>
2021-11-15 14:00:36 +0100

    css final

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -8,7 +8,7 @@ const Reviews = ({reviews}) => {
         {
             reviews.reviews.map((review) => {
                 return(
-                    <div key={review.id} className="card text-white bg-primary mb-3 mr-4" style={{maxWidth: '30%'}}>
+                    <div key={review.id} className=" card text-white bg-primary mb-2 mx-1" style={{maxWidth: '30%'}}>
                         <div className="card-header d-flex justify-content-between">
                             <span>{review.name}</span>
                             <span><StarRating rating={review.rating} /></span>
@@ -20,36 +20,6 @@ const Reviews = ({reviews}) => {
                 )
             })
         }
-
-            {/* <div className="card text-white bg-primary mb-3 mr-4" style={{maxWidth: '30%'}}>
-                <div className="card-header d-flex justify-content-between">
-                    <span>Joan</span>
-                    <span><StarRating rating={3} /></span>
-                </div>
-                <div className="card-body">
-                    <p className="card-text"> the restaurant was awesome</p>
-                </div>
-            </div>
-
-            <div className="card text-white bg-primary mb-3 mr-4" style={{maxWidth: '30%'}}>
-                <div className="card-header d-flex justify-content-between">
-                    <span>Joan</span>
-                    <span><StarRating rating={3} /></span>
-                </div>
-                <div className="card-body">
-                    <p className="card-text"> the restaurant was awesome</p>
-                </div>
-            </div>
-
-            <div className="card text-white bg-primary mb-3 mr-4" style={{maxWidth: '30%'}}>
-                <div className="card-header d-flex justify-content-between">
-                    <span>Joan</span>
-                    <span><StarRating rating={3} /></span>
-                </div>
-                <div className="card-body">
-                    <p className="card-text"> the restaurant was awesome</p>
-                </div>
-            </div> */}
         </div>
     )
 }
